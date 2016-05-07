60111dc21c18e937c157a668cddd7e5d1e8867e5
Svenja <Svenja@Svenjas_PC.local>
2016-05-07 18:47:25 +0200

    Validierung funktioniert noch nicht, var eingefügt für Zeilen in Tabelle

--- Meilenstein_1/FormValidate.js
+++ Meilenstein_1/FormValidate.js
@@ -5,6 +5,16 @@ var regexGeb = ([1900-2015]);
 
 
 
+var vorname = document.getElementsByName('vorname')[0].value;
+var nachname = document.getElementsByName('nachname')[0].value;
+var verein = document.getElementsByName('verein')[0].value;
+var headcoach = document.getElementsByName('headcoach')[0].value;
+var asscoach = document.getElementsByName('asscoach')[0].value;
+var rnr = parseInt(document.getElementsByName('rnr')[0].value);
+var geb = parseInt(document.getElementsByName('geb')[0].value);
+
+
+
 function validateForm() {
     
     if (regexNames.test(vorname) === false) {
