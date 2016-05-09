b63c173ab9fb0ca6b043d90af1404a25eaa5568f
Svenja <Svenja@Svenjas_PC.local>
2016-05-09 11:24:42 +0200

    Link für xhr request

--- Meilenstein_1/FormValidate.js
+++ Meilenstein_1/FormValidate.js
@@ -3,12 +3,12 @@ var regexNames = ([A-Za-zäÄöÖüÜß]);
 var regexRuecken = ([4-15]);
 var regexGeb = ([1900-2015]);
 
-
+//http://www.html5rocks.com/de/tutorials/file/xhr2/
 
 var vorname = document.getElementsByName('vorname')[0].value;
 var nachname = document.getElementsByName('nachname')[0].value;
 var verein = document.getElementsByName('verein')[0].value;
-var headcoach = document.getElementsByName('headcoach')[0].value;
+var headcoach = document.getElementsByName('hcoach')[0].value;
 var asscoach = document.getElementsByName('asscoach')[0].value;
 var rnr = parseInt(document.getElementsByName('rnr')[0].value);
 var geb = parseInt(document.getElementsByName('geb')[0].value);
