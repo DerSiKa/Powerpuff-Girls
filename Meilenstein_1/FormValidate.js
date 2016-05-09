1ae3dc12a6bcd1adc66e8daf9228a1e02b17f190
student <student@192.168.101.26>
2016-05-09 15:34:37 +0200

    JSON zu HTML --> jQuery drawTable validateForm() verbessert (Klammern, etc.) Toggle Button mit Buttons umgesetzt

--- Meilenstein_1/FormValidate.js
+++ Meilenstein_1/FormValidate.js
@@ -1,9 +1,8 @@
 
-var regexNames = /^[a-zA-Z]+$/;
+var regexNames = /^[a-zäöü]+$/i;
 var regexRuecken = ([4-15]);
 var regexGeb = ([1900-2015]);
 
-//http://www.html5rocks.com/de/tutorials/file/xhr2/
 
 var vorname = document.getElementsByName('vorname')[0].value;
 var nachname = document.getElementsByName('nachname')[0].value;
@@ -22,52 +21,107 @@ function validateForm() {
         return false;
     }
     
-    if (regexNames.test(nachname) === false) {
+    else if (regexNames.test(nachname) === false) {
         alert("Einige Angaben sind fehlerhaft. Bitte überprüfen Sie Ihre Eingaben.");
         return false;
     }
     
-    if (regexNames.test(verein) === false) {
+    else if (regexNames.test(verein) === false) {
         alert("Einige Angaben sind fehlerhaft. Bitte überprüfen Sie Ihre Eingaben.");
         return false;
     }
     
-    if (regexNames.test(headcoach) === false) {
+    else if (regexNames.test(headcoach) === false) {
         alert("Einige Angaben sind fehlerhaft. Bitte überprüfen Sie Ihre Eingaben.");
         return false;
     }
     
-    if (regexNames.test(asscoach) === false) {
-        alert("Einige Angaben sind fehlerhaft. Bitte überprüfen Sie Ihre Eingaben.");
-        return false;
-    
-    if (regexRuecken.test(rnr) === false) {
+    else if (regexNames.test(asscoach) === false) {
         alert("Einige Angaben sind fehlerhaft. Bitte überprüfen Sie Ihre Eingaben.");
         return false;
     }
     
-    if (regexGeb.test(geb) === false) {
+    else if (regexRuecken.test(rnr) === false) {
         alert("Einige Angaben sind fehlerhaft. Bitte überprüfen Sie Ihre Eingaben.");
         return false;
     }
     
+    else if (regexGeb.test(geb) === false) {
+        alert("Einige Angaben sind fehlerhaft. Bitte überprüfen Sie Ihre Eingaben.");
+        return false;
+    }
+    return true;
+    }
  
  
  
  
  
  	function sendForm(form) {
-  	var formData = new FormData(form);
-  	var xhr = new XMLHttpRequest();
-  	xhr.open('POST', form.action, true);
-  	xhr.onload = function(e) { ... };
-  	xhr.send(formData);
-  	return false;
-	}
+ 		if (validateForm()){
+ 			var formData = new FormData(form);
+ 			var xhr = new XMLHttpRequest();
+ 			xhr.open('POST', 'http://139.59.134.26/', true);
+ 			xhr.onload = function(e) { ... };
+ 			xhr.send(formData);
+ 		}
+ 	}
+ 	
+ 	Window.onLoad = function () {
+ 	        drawTable('http://139.59.134.26/api/players'); 
+ 	}
+
+ 	function drawTable(link) { 
+  // Vorherige Daten  aus Tabelle löschen 
+ 		   //$(".data").remove();  TODO raus
+ 	 
+ 		 
+ 		  var xhr2= new XMLHttpRequest();
+ 	 		xhr2.open('GET', link, true);
+ 	 		xhr2.send();
+ 	 		xhr2.onreadystate = function() {
+ 	 			if (xhr2.readyState == 4) {
+ 	 				if (xhr2.status == 200) {
+ 	 					var data = JSON.parse(xhr2.response); 
+ 	 					for (var i = 0; i < data.length; i++) { 
+ 	 		                drawRow(data[i]); 
+ 	 		             } 
+ 	 				} else {
+ 	 					alert('Es konnte keine Verbindung hergestellt werden!');
+ 	 				}
+ 	 			}
+ 	 		}
+ 		 } 
+ 		 
+ 		 
+ 	function drawRow(rowData) { 
+ 		   var row = $("<tr class='data' />"); //TODO Siehe oben remove();
+ 	       $("#tabelle").append(row); 
+ 		
+ 		 
+ 		  row.append($("<td>" + rowData.vorname + " " + rowData.name + "</td>")); 
+ 		  row.append($("<td>" + rowData.club + "</td>")); 
+ 		  row.append($("<td>" + rowData.coach + "</td>")); 
+ 		  row.append($("<td>" + rowData.position + "</td>")); 
+ 		  row.append($("<td>" + rowData.number + "</td>")); 
+ 		  row.append($("<td>" + rowData.year + "</td>")); 
+ 	 } 
 
 
+// TODO	function toggle(name)
+//    {
+//    if (favorites) {
+//   
+//    	GET/api/players?favorites=true
+//    		
+//       document.getElementById("loginTable").style.display="table";
+//       document.getElementById("loginLink").onclick = toggleTable(false);
+//
+//    } else {
+//    	GET /api/players
+//       document.getElementById("loginTable").style.display="none";
+//       document.getElementById("loginLink").onclick = toggleTable(true);
+//    }
+//   }
 
-	function toggle(){
-	//bei klick nur favoriten zeigen
-	}
-}
+
